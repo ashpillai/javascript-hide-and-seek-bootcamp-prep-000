3aa10aa4dbe03243919f147fbe98baa0bc46d6e6
pillai.ashwini@gmail.com <pillai.ashwini@gmail.com>
2018-07-20 11:26:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('.ranked-list li');
+  for(let i=0; i<lis.length;i++){
+    parseInt(lis[i])+parseInt(n);
+  }
+}
+
+function deepestChild() {
+  document.
+}
